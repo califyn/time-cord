4ca32e64b36b899b62d0d6f63f3255c1a25445d6
Ali Cy <califynic@gmail.com>
2020-10-05 23:00:28 -0700

    Added source of the code

--- time-cord/main.py
+++ time-cord/main.py
@@ -1,3 +1,6 @@
+
 from AppKit import NSWorkspace
 active_app_name = NSWorkspace.sharedWorkspace().frontmostApplication().localizedName()
 print(active_app_name)
+
+# Code credit: kmundnic's answer on https://stackoverflow.com/questions/373020/finding-the-current-active-window-in-mac-os-x-using-python?noredirect=1&lq=1
