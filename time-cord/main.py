0de3121063e7388acf50b4f303437d0d9b39d315
Ali Cy <califynic@gmail.com>
2020-10-05 23:46:31 -0700

    Fixed getting name of focused window

--- time-cord/main.py
+++ time-cord/main.py
@@ -1,6 +1,29 @@
+from subprocess import Popen, PIPE
 
-from AppKit import NSWorkspace
-active_app_name = NSWorkspace.sharedWorkspace().frontmostApplication().localizedName()
-print(active_app_name)
+# Returns name & title of the focused window in the format "name, title".
+def return_top():
+    frontapp = '''
+        global frontApp, frontAppName, windowTitle
 
-# Code credit: kmundnic's answer on https://stackoverflow.com/questions/373020/finding-the-current-active-window-in-mac-os-x-using-python?noredirect=1&lq=1
+        set windowTitle to ""
+        tell application "System Events"
+            set frontApp to first application process whose frontmost is true
+            set frontAppName to name of frontApp
+            tell process frontAppName
+                tell (1st window whose value of attribute "AXMain" is true)
+                    set windowTitle to value of attribute "AXTitle"
+                end tell
+            end tell
+        end tell
+
+        return {frontAppName, windowTitle}
+      '''
+
+    proc = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    front, error = proc.communicate(frontapp)
+    return front
+# Code credits:
+# Albert's answer, https://stackoverflow.com/questions/5292204/macosx-get-foremost-window-title
+# RobC's answer, https://stackoverflow.com/questions/51775132/how-to-get-return-value-from-applescript-in-python
+
+print(return_top())
